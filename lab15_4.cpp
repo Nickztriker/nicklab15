288edf5e9c5c5713fe4cf9c888d9de49f3d3d84c
Nickztriker <nick@coder.com>
2024-01-26 16:33:07 +0700

    Completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,18 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int salub[] = {a,b,c,d};
+	int N = sizeof(salub)/sizeof(salub[0]);
+
+	for(int i=0; i<N; i++){
+		int j = rand()%N;
+		swap(salub[i],salub[j]);
+	}
+	a = salub[0];
+	b = salub[1];
+	c = salub[2];
+	d = salub[3];
+	
+}
--- lab15_5.cpp
+++ lab15_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *,int *,int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,24 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a,int *b,int *c,int *d){
+	int salub[] = {*a,*b,*c,*d};
+	int N = sizeof(salub)/sizeof(salub[0]);
+
+	for(int i=0; i<N; i++){
+		int j = rand()%N;
+		swap(salub[i],salub[j]);
+	}
+	*a = salub[0];
+	*b = salub[1];
+	*c = salub[2];
+	*d = salub[3];
+
+}
